cc8e3aa020b0a427fd0503cf712077d9b4a69bba
gaa <tenkiyohou69@gmail.com>
2019-04-19 11:41:21 +0900

    ルートパラメーターをテンプレートに渡す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -23,69 +23,10 @@ function tag($tag, $txt) {
 
 class HelloController extends Controller {
 
-    public function index($id = 'zero') {
+    public function index(Request $request) {
         $data = ['msg' => 'これはコントローラから渡されたメッセージです。',
-            'id' => $id
+            'id' => $request->id
         ];
         return view('hello.index', $data);
     }
-
-//    public function index(Request $request, Response $response) {
-//
-//        global $head, $style, $body, $end;
-//
-//        $html = <<<EOF
-//                <html>
-//                <head>
-//                <style>
-//                body{font-size:16pt; color:#999;}
-//                h1{font-size:120pt;text-align:right; color:#fafafa;}
-//                </style>
-//                </head>
-//                <body>
-//                    <h1>Hello</h1>
-//                    <h3>Request</h3>
-//                    <pre>{$request}</pre>
-//                    <h3>Response</h3>
-//                    <pre>{$response}</pre>
-//
-//                </body>
-//                </html>
-//                EOF;
-//        $response->setContent($html);
-//        return $response;
-//    }
-//      シングルアクションコントローラー
-//    public function __invoke() {
-//        global $head, $style, $body, $end;
-//        
-//        $html = $head. tag('title', 'Hello/Index').$style.
-//                $body.tag('h1', 'Index').tag('p', 'this is Index page')
-//                .'<a href="/hello/other">go to Other page6666</a>'
-//                .$end;
-//        return $html;
-//        
-//    }
-//    public function index() {
-//        global $head, $style, $body, $end;
-//        
-//        $html = $head. tag('title', 'Hello/Index').$style.
-//                $body.tag('h1', 'Index').tag('p', 'this is Index page')
-//                .'<a href="/hello/other">go to Other page</a>'
-//                .$end;
-//        return $html;
-//
-//    }
-//    
-//    public function other() {
-//        global $head, $style, $body, $end;
-//        
-//        $html = $head .
-//                tag('title','Hello/Other')
-//                .$style.
-//                $body
-//                .tag('h1','Other').tag('p', 'this is  Other page')
-//                .$end;
-//        return $html;
-//    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 //    return view('hello.index');
 //});
 
-Route::get('hello/{id?}','HelloController@index');
+Route::get('hello/','HelloController@index');
 //Route::get('hello/other','HelloController@other');
 //Route::get('hello','HelloController');
